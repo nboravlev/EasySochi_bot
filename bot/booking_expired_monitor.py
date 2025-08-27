fd74f7e2aab0e26a64b031cd2f6bab571833a6bc
ML_GOD <n.boravlev@gmail.com>
2025-08-27 17:19:41 +0000

    Stop tracking local test configuration files

---
+++ bot/booking_expired_monitor.py
@@ -0,0 +1,116 @@
+
+from datetime import timedelta
+from sqlalchemy import select, func, and_
+from sqlalchemy.orm import selectinload
+from db.db_async import get_async_session
+from db.models.bookings import Booking
+from db.models.apartments import Apartment
+from db.models.users import User
+
+from utils.logging_config import log_function_call, LogExecutionTime, get_logger
+
+# Константы
+TARGET_BOOKING_STATUS = 5      # "ожидает подтверждения"
+BOOKING_STATUS_TIMEOUT = 11    # "время истекло"
+
+
+@log_function_call(action="check_expired_booking")
+async def check_expired_booking(context):
+    """Проверка и обработка просроченных броней"""
+    logger = get_logger(__name__)
+    bot = context.bot
+
+    try:
+        async with get_async_session() as session:
+            stmt = (
+                select(Booking)
+                .options(
+                    selectinload(Booking.apartment)
+                    .selectinload(Apartment.owner),
+                    selectinload(Booking.user)
+                )
+                .where(
+                    and_(
+                    Booking.status_id == TARGET_BOOKING_STATUS,
+                    func.now() - Booking.created_at > timedelta(hours=24),
+                    Booking.is_active == True
+                    )
+                )
+            )
+
+            result = await session.execute(stmt)
+            expired_bookings = result.scalars().all()
+
+            if not expired_bookings:
+                logger.info(
+                    f"No expired bookings found for status={TARGET_BOOKING_STATUS} "
+                    f"and timeout=24h",
+                    extra={"action": "check_expired_booking"}
+                )
+            else:
+                # Ограничиваем количество выводимых ID до 10, если их много
+                booking_ids = [b.id for b in expired_bookings[:10]]
+                if len(expired_bookings) > 10:
+                    booking_ids.append("...")
+                
+                logger.info(
+                    f"Found {len(expired_bookings)} expired bookings to process",
+                    extra={
+                        "action": "check_expired_booking",
+                        "booking_ids": booking_ids
+                    }
+                )
+
+            for booking in expired_bookings:
+                booking.status_id = BOOKING_STATUS_TIMEOUT
+                await session.commit()
+
+
+                                # Замер времени уведомлений
+                with LogExecutionTime("notify_timeout", logger, user_id=booking.user.id, chat_id=booking.apartment.owner.id):
+                    await notify_timeout(bot, booking)
+
+    except Exception as e:
+        logger.exception(
+            f"Ошибка при обработке просроченных броней: {e}",
+            extra={"action": "check_expired_booking"}
+        )
+
+
+async def notify_timeout(bot, booking):
+    """Отправка уведомлений о том, что бронь истекла"""
+    logger = get_logger(__name__)
+    guest_chat_id = booking.tg_user_id
+    owner_chat_id = booking.apartment.tg_user_id
+    created_local = (booking.created_at + timedelta(hours=3)).replace(second=0, microsecond=0)
+
+# Форматируем для сообщения
+    created_str = created_local.strftime("%Y-%m-%d %H:%M")
+
+    guest_text = (
+        f"⏰ Ваш запрос на бронирование объекта <b>{booking.apartment.short_address}</b>\n"
+        f"С: {booking.check_in} по: {booking.check_out}\n"
+        f"отменен, так как истек срок ожидания подтверждения.\n"
+        f"Хотите создать новое бронирование? /start"
+    )
+
+    owner_text = (
+        f"⏰ Запрос на бронирование объекта <b>{booking.apartment.short_address}</b>,\n"
+        f"Созданный и направленный вам для подтверждения: {created_str},\n"
+        f"Стоимостью {booking.total_price} р.<b>‼️отменен‼️</b>\n"
+        f"⌛️ Истек срок 24 часа на подтверждение"
+    )
+
+    await bot.send_message(chat_id=guest_chat_id, text=guest_text, parse_mode="HTML")
+    await bot.send_message(chat_id=owner_chat_id, text=owner_text, parse_mode="HTML")
+
+
+    logger.info(
+        f"Timeout notifications sent for booking {booking.id}",
+        extra={
+            "action": "notify_timeout",
+            "booking_id": booking.id,
+            "guest_chat_id": guest_chat_id,
+            "owner_chat_id": owner_chat_id
+        }
+    )
