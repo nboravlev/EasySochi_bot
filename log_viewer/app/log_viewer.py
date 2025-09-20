59ba8242a3703082236530d5fd36c1ec913ac1f8
Nikolai_Boravlev <n.boravlev@gmail.com>
2025-09-20 19:04:00 +0300

    Update log_viewer.py

--- log_viewer/app/log_viewer.py
+++ log_viewer/app/log_viewer.py
@@ -229,7 +229,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "log_viewer:app",
         host="0.0.0.0",
-        port=8888,
+        port=8080,
         log_level="info",
         reload=False
     )
